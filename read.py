029db6d3f611bec107b404e86c2d903e26dddc1d
N4SSER <nasserbrown@estudiantec.cr>
2022-12-27 19:14:48 -0600

    load datasets

---
+++ read.py
@@ -0,0 +1,86 @@
+import os
+from tqdm import tqdm
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.io import wavfile
+from python_speech_features import mfcc, logfbank
+from librosa import load
+def plot_signals(signals):
+    fig, axes = plt.subplots(nrows=2, ncols=5, sharex=False,
+                             sharey=True, figsize=(20,5))
+    fig.suptitle('Time Series', size=16)
+    i = 0
+    for x in range(2):
+        for y in range(5):
+            axes[x,y].set_title(list(signals.keys())[i])
+            axes[x,y].plot(list(signals.values())[i])
+            axes[x,y].get_xaxis().set_visible(False)
+            axes[x,y].get_yaxis().set_visible(False)
+            i += 1
+
+def plot_fft(fft):
+    fig, axes = plt.subplots(nrows=2, ncols=5, sharex=False,
+                             sharey=True, figsize=(20,5))
+    fig.suptitle('Fourier Transforms', size=16)
+    i = 0
+    for x in range(2):
+        for y in range(5):
+            data = list(fft.values())[i]
+            Y, freq = data[0], data[1]
+            axes[x,y].set_title(list(fft.keys())[i])
+            axes[x,y].plot(freq, Y)
+            axes[x,y].get_xaxis().set_visible(False)
+            axes[x,y].get_yaxis().set_visible(False)
+            i += 1
+
+def plot_fbank(fbank):
+    fig, axes = plt.subplots(nrows=2, ncols=5, sharex=False,
+                             sharey=True, figsize=(20,5))
+    fig.suptitle('Filter Bank Coefficients', size=16)
+    i = 0
+    for x in range(2):
+        for y in range(5):
+            axes[x,y].set_title(list(fbank.keys())[i])
+            axes[x,y].imshow(list(fbank.values())[i],
+                    cmap='hot', interpolation='nearest')
+            axes[x,y].get_xaxis().set_visible(False)
+            axes[x,y].get_yaxis().set_visible(False)
+            i += 1
+
+def plot_mfccs(mfccs):
+    fig, axes = plt.subplots(nrows=2, ncols=5, sharex=False,
+                             sharey=True, figsize=(20,5))
+    fig.suptitle('Mel Frequency Cepstrum Coefficients', size=16)
+    i = 0
+    for x in range(2):
+        for y in range(5):
+            axes[x,y].set_title(list(mfccs.keys())[i])
+            axes[x,y].imshow(list(mfccs.values())[i],
+                    cmap='hot', interpolation='nearest')
+            axes[x,y].get_xaxis().set_visible(False)
+            axes[x,y].get_yaxis().set_visible(False)
+            i += 1
+
+
+df = pd.read_csv('instruments.csv')
+df.set_index('fname',inplace=True)
+
+for f in df.index:
+    sample_rate, x = wavfile.read('wavfiles/'+f)
+    df.at[f,'length'] = x.shape[0]/sample_rate
+
+classes = list(np.unique(df.label))
+
+class_dist = df.groupby(['label'])['length'].mean()
+
+fig, ax = plt.subplots()
+ax.set_title('Class Distribution')
+ax.pie(class_dist,labels=class_dist.index, autopct = '%1.1f%%',shadow=False,startangle=90)
+ax.axis('equal')
+plt.show()
+df.reset_index(inplace=True)
+
+for c in classes:
+    wav_file = df[df.label == c].iloc[0,0]
+    x,sample_rate = load('wavefiles/'+wav_file,sr=44100)
