05939856aba8ab0ecdd1658799afbd652dae28b2
Nasser <neyss08@hotmail.com>
2023-01-08 17:52:11 -0600

    finished models

--- read.py
+++ read.py
@@ -63,6 +63,19 @@ def plot_mfccs(mfccs):
             i += 1
 
 
+
+def envelope(x,Fs,t):
+    mask = []
+    x = pd.Series(x).apply(np.abs)
+    x_mean = x.rolling(window = int(Fs/10),min_periods=1,center = True).mean()
+    for mean in x_mean:
+        if mean > t:
+            mask.append(True)
+        else:
+            mask.append(False)
+
+    return mask
+
 df = pd.read_csv('instruments.csv')
 df.set_index('fname',inplace=True)
 
@@ -78,9 +91,40 @@ fig, ax = plt.subplots()
 ax.set_title('Class Distribution')
 ax.pie(class_dist,labels=class_dist.index, autopct = '%1.1f%%',shadow=False,startangle=90)
 ax.axis('equal')
-plt.show()
+#plt.show()
 df.reset_index(inplace=True)
+signals={}
+fft = {}
+fbank = {}
+mfccs ={}
+
+def _fft(x,Fs):
+    N = len(x)
+    freqs = np.fft.rfftfreq(len(x),d=1/sample_rate)
+    X = abs(np.fft.rfft(x)/N)
+    return (X,freqs)
 
 for c in classes:
     wav_file = df[df.label == c].iloc[0,0]
-    x,sample_rate = load('wavefiles/'+wav_file,sr=44100)
+    x,sample_rate = load('wavfiles/'+wav_file,sr=44100)
+    mask = envelope(x,sample_rate,0.0005)
+    x = x[mask]
+    signals[c] = x
+    fft[c] = _fft(x,sample_rate)
+    bank = logfbank(x[:sample_rate],sample_rate,nfilt=26,nfft = 1103).T
+    fbank[c] = bank
+    mel = mfcc(x[:sample_rate],sample_rate,numcep=13,nfilt=26,nfft=1103).T
+    mfccs[c] = mel
+
+plot_signals(signals)
+#plt.show()
+plot_fft(fft)
+#plt.show()
+plot_mfccs(mfccs)
+#plt.show()
+
+if len(os.listdir('clean')) == 0:
+    for f in tqdm(df.fname):
+        x,sample_rate = load('wavfiles/'+f,sr=16000)
+        mask = envelope(x,sample_rate,t = 0.0005)
+        wavfile.write(filename='clean/'+f,rate=sample_rate,data=x[mask])
